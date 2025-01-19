4be3ddcd49bcb9c204e31503ff72f9508047eb0f
unknown <mrarifav@gmail.com>
2025-01-19 13:46:19 -0600

    Sistema busqueda basico

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,48 @@
-//EJEMPLO DE LA DOCUMENTACION
-//FUENTE: https://www.meilisearch.com/docs/guides/front_end/react_quick_start
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MeiliSearch } from 'meilisearch';
 
 const client = new MeiliSearch({
-  host: 'http://18.227.13.140', 
+  host: 'http://18.227.13.140',
   apiKey: 'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be',
 });
 
-const busqueda = () => {
-  const [searchQuery, setSearchQuery] = useState(''); 
-  const [searchResults, setSearchResults] = useState([]); 
+const BusquedaMEILI = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  const busquedayresultados = async () => {
-    const index = client.index('movies'); 
-    const response = await index.search(searchQuery); 
-    setSearchResults(response.hits); 
-  };
+  useEffect(() => {
+    const busquedayresultados = async () => {
+      const index = client.index('movies');
+      const response = await index.search(searchQuery);
+      setSearchResults(response.hits);
+    };
+
+    if (searchQuery) {
+      busquedayresultados();
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchQuery]);
 
   return (
     <div>
-      <p>TEST</p>
+      <h1>ARI CINEMA.tv</h1>
+      <input
+        type="text"
+        placeholder="Buscar película"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      <ul>
+        {searchResults.map((movie) => (
+          <li key={movie.id}>
+            <p>{movie.title} - {movie.genres}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default busqueda;
+export default BusquedaMEILI;
